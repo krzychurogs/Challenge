c56396d08670886675a4e0be6c42b565e766a50e
krzychurogs <krzychurogs@tlen.pl>
2020-10-11 14:14:24 +0200

    polygon func

--- app/src/main/java/com/example/challenge/FragmentRoad.java
+++ app/src/main/java/com/example/challenge/FragmentRoad.java
@@ -1,149 +1,209 @@
 package com.example.challenge;
 
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.SystemClock;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.auth.User;
 
-import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class FragmentRoad extends Fragment {
-
+public class FragmentRoad extends Fragment implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks,com.google.android.gms.location.LocationListener, GoogleApiClient.OnConnectionFailedListener{
+    DatabaseReference reff;
+    private FirebaseAuth mAuth;
     private GoogleMap mMap;
     GoogleApiClient mGoogleApiClient;
     Location mLastLocation;
-    Location mFirstLocation;
     LocationRequest mLocationRequest;
+    Location flocation;
+    private FragmentRoadListener listener;
+    Button next,back;
+    TextView textdistance,textspeed;
+    private LatLng lastKnownLatLng;
+    List<String>listofplace=new ArrayList<String>();
+    List<String>distancelist=new ArrayList<String>();
     private Polyline gpsTrack;
-    private FirebaseAuth mAuth;
-    private LatLng lastKnownLatLng,endKnownLatLng,firstKnownLatLng;
-    Button MStart,MEnd;
-    Button mStopTime,mResetTime;
-    EditText MDistance,MSpeed;
-    TextView MCzas;
-    int secs;
-    DrawerLayout drawerLayout;
+    List<String>speedlist=new ArrayList<String>();
+    List<String>listofeachtrainingwaypoint=new ArrayList<>();
+    public static int counttrain;
+    List<LatLng> points ;
 
 
-    Handler customHandler=new Handler();
-    Runnable updateTimerThread=new Runnable() {
-        @Override
-        public void run() {
-            timeinMiliseconds=SystemClock.uptimeMillis()-startTime;
-            updateTime=timeinMiliseconds+timeSwapBuff;
-            secs=(int)(updateTime/1000);
-            int mins=secs/60;
-            secs%=60;
-            int milliseconds=(int)(updateTime%1000);
-            MCzas.setText(""+mins+":"
-                    +String.format("%2d",secs)+":"
-                    +String.format("%3d",milliseconds));
-            customHandler.postDelayed(this,0);
-        }
-    };
-    long startTime=0L,timeinMiliseconds=0L,timeSwapBuff=0L,updateTime=0L;
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
-    {
-        return  inflater.inflate(R.layout.fragment_road,container,false);
-
+    public interface FragmentRoadListener{
+        void onInputSent(CharSequence input);
     }
+    @Nullable
     @Override
-    public void onStart() {
-        super.onStart();
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+        String user_id = mAuth.getCurrentUser().getUid();
+
+
+        final View root = inflater.inflate(R.layout.fragment_road, container, false);
 
 
 
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager()
+                .findFragmentById(R.id.map2);
+        mapFragment.getMapAsync(this);
 
 
+
+        return root;
 
     }
 
 
 
+    protected synchronized void buildGoogleApiClient(){
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity().getApplicationContext())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        buildGoogleApiClient();
+        mMap.setMyLocationEnabled(true);
+        PolylineOptions polylineOptions = new PolylineOptions();
+        polylineOptions.color(Color.CYAN);
+        polylineOptions.width(4);
+        gpsTrack = mMap.addPolyline(polylineOptions);
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(10000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Polygon polygon = mMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(50.03,21.9987751), new LatLng(50.0227154,21.9997975), new LatLng(50.002724,21.9927758), new LatLng(50.022724,21.9527758))
+                .strokeColor(Color.RED)
+                .fillColor(Color.BLUE));
+        lastKnownLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+        final List<LatLng> latLngPolygon = new ArrayList<>();
+        {
+            latLngPolygon.add(new LatLng(50.03,21.9987751));//delhi
+            latLngPolygon.add(new LatLng(50.0227154,21.9997975));//gujarat
+            latLngPolygon.add(new LatLng(50.002724,21.9927758));//pune
+            latLngPolygon.add(new LatLng(50.022724,21.9527758));
+        }
+        System.out.println("w srodku"+isPointInPolygon(lastKnownLatLng, (ArrayList<LatLng>) latLngPolygon));
 
 
-
-
-
-
-    private void updateTrack() {
-        List<LatLng> points = gpsTrack.getPoints();
-        final Location first=new Location("");
-        final Location second=new Location("");
-        points.add(lastKnownLatLng);
-        float[] distance = new float[3];
-        float suma=0;
-
-        Location.distanceBetween( first.getLatitude(), first.getLongitude(),
-                second.getLatitude(), second.getLongitude(), distance);
-
-        for(int i=0;i<points.size()-1;i++){
-
-
-            first.setLatitude(points.get(i).latitude);
-            first.setLongitude(points.get(i).longitude);
-            second.setLatitude(points.get(i+1).latitude);
-            second.setLongitude(points.get(i+1).longitude);
-            Location.distanceBetween( first.getLatitude(), first.getLongitude(),
-                    second.getLatitude(), second.getLongitude(), distance);
-
-            suma+=distance[0];
-            System.out.println(i+"dystans to"+distance[0]);
-            System.out.println(i+"suma to"+suma);
-
-            MDistance.setText("Dystans to:"+ suma);
-            double km= 3.6;
-            final double avgspeed= (suma/secs)* km ;
-            DecimalFormat df = new DecimalFormat("#.##");
-            MSpeed.setText("Predkosc to:"+df.format(avgspeed)+"km/h");
-            final double finalSuma = suma;
-            MEnd.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View view) {
-
-                    String user_id = mAuth.getCurrentUser().getUid();
-                    DecimalFormat df = new DecimalFormat("#.##");
-
-                    String szybkosc= df.format(avgspeed).toString();
-                    Toast.makeText(getActivity().getApplicationContext(),"Dobry trening",Toast.LENGTH_SHORT).show();
-                    DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child
-                            ("Customers").child("Historia").child(user_id).child("predkosc");
-                    current_user_db.setValue(szybkosc);
-                    DatabaseReference distancer = FirebaseDatabase.getInstance().getReference().child("Users").child
-                            ("Customers").child("Historia").child(user_id).child("dystans");
-                    distancer.setValue(finalSuma);
-
-
-                }
-            });
+    }
+    private boolean isPointInPolygon(LatLng tap, ArrayList<LatLng> vertices) {
+        int intersectCount = 0;
+        for (int j = 0; j < vertices.size() - 1; j++) {
+            if (rayCastIntersect(tap, vertices.get(j), vertices.get(j + 1))) {
+                intersectCount++;
+            }
         }
 
+        return ((intersectCount % 2) == 1); // odd = inside, even = outside;
+    }
 
-        gpsTrack.setPoints(points);
+    private boolean rayCastIntersect(LatLng tap, LatLng vertA, LatLng vertB) {
 
+        double aY = vertA.latitude;
+        double bY = vertB.latitude;
+        double aX = vertA.longitude;
+        double bX = vertB.longitude;
+        double pY = tap.latitude;
+        double pX = tap.longitude;
 
+        if ((aY > pY && bY > pY) || (aY < pY && bY < pY)
+                || (aX < pX && bX < pX)) {
+            return false; // a and b can't both be above or below pt.y, and a or
+            // b must be east of pt.x
+        }
+
+        double m = (aY - bY) / (aX - bX); // Rise over run
+        double bee = (-aX) * m + aY; // y = mx + b
+        double x = (pY - bee) / m; // algebra is neat!
+
+        return x > pX;
+    }
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if(context instanceof FragmentRoadListener){
+            listener= (FragmentRoadListener) context;
+
+        }
+        else {
+            throw new RuntimeException(context.toString()+" trzeba implementowac Listnera");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener=null;
     }
 
 }
